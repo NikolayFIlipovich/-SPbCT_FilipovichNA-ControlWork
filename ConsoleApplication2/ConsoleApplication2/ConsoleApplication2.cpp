2f8190a077220d0c55f8c1d9da729e5b14817ef7
Nikolay Filipovich <74007295+NikolayFIlipovich@users.noreply.github.com>
2020-11-05 18:10:41 +0300

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string>
+#include <stdio.h>
+#include <iostream>
+int read(char* filename);
+int main()
+{
+	std::string stringfilename;
+	std::cout << "Input your path: ";
+	std::cin >> stringfilename;
+	char* filename = (char*)stringfilename.c_str();
+	read(filename);
+	return 0;
+
+}
+
+int read(char* filename)
+{
+	FILE* fp;
+	char c;
+	if ((fp = fopen(filename, "r")) == NULL)
+	{
+		perror("Error");
+		return 1;
+	}
+	while ((c = fgetc(fp)) != EOF)
+	{
+		printf("%c ", c);
+	}
+
+	fclose(fp);
+	return 0;
+}
